c067bc0ddc362eaa7dce0d7d91e65efe19265165
AngusLin <angus848241995@gmail.com>
2016-11-16 11:53:45 +0800

    Outdated

--- mcts.py
+++ mcts.py
@@ -1,2 +1,21 @@
 import random
 import math
+
+
+def UCTSEARCH(budget,root):
+	for iter in range(budget):
+		if iter%10000==9999:
+			logger.info("simulation: %d"%iter)
+			logger.info(root)
+		front=TREEPOLICY(root)
+		reward=DEFAULTPOLICY(front.state)
+		BACKUP(front,reward)
+	return BESTCHILD(root,0)
+
+def TREEPOLICY(node):
+	while node.state.terminal()==False:
+		if node.fully_expanded()==False:	
+			return EXPAND(node)
+		else:
+			node=BESTCHILD(node,SCALAR)
+	return node
